ad1e4fd6e2799bcb3719dbc3dc82c6d998229073
v587wxy <937129278@qq.com>
2026-04-23 14:56:48 +0800

    修改了切换阀的检查机制

--- Drivers/SerialDevices/SwitchValveController.py
+++ Drivers/SerialDevices/SwitchValveController.py
@@ -21,14 +21,37 @@ class SwitchValve:
     def move_to_position(self, position: int):
         time.sleep(1)
         data = bytes([self.address, 0x05, 0x00, position,0xFF,0x00])
-        recv = self.serial_port.sendcmd(
+        self.serial_port.sendcmd(
             data, 8, validator=self._modbus_validator(0x05), retries=2, retry_delay_s=0.1
         )
         time.sleep(2)
-        # 读取一次位置直到到达目标
+        # 读取一次位置；若读空则重发位置命令，若收到 11 03 EE 开头则先复位后再执行原位置命令
+        query_data = bytes([self.address, 0x04, 0x00,0x00, 0x00,0x02])
+        query_recv = self.serial_port.sendcmd(query_data, 9, retries=2, retry_delay_s=0.1)
+
+        if not query_recv:
+            self.serial_port.sendcmd(
+                data, 8, validator=self._modbus_validator(0x05), retries=2, retry_delay_s=0.1
+            )
+            time.sleep(2)
+        elif query_recv.startswith(bytes([0x11, 0x03, 0xEE])):
+            self.reset()
+            time.sleep(3)
+            self.serial_port.sendcmd(
+                data, 8, validator=self._modbus_validator(0x05), retries=2, retry_delay_s=0.1
+            )
+            time.sleep(2)
+
         pos = self.query_position()
         print(f'当前位置: {pos}, 目标位置: {position}')
         return pos == position
+
+    def reset(self):
+        data = bytes([self.address, 0x05, 0x00, 0x00, 0xFF, 0x00])
+        recv = self.serial_port.sendcmd(
+            data, 8, validator=self._modbus_validator(0x05), retries=2, retry_delay_s=0.1
+        )
+        return len(recv) == 8
 
 
     def query_position(self):
